de8ca6975ea7e83cd1dcc213d7fdf3da2408c325
Gaurav Gurung <gauravgurung067@gmail.com>
2022-04-11 10:36:05 +0545

    add feature to prevent too many failed login attempts in adminLogin

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Admin\Auth;
 
+use Illuminate\Auth\Events\Lockout;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -18,6 +22,7 @@ class LoginController extends Controller
      * @return void
      */
     function loginProcess(Request $request){
+        $this->ensureIsNotRateLimited($request);
          $request->validate([
             'email'=>'required|email|exists:admins,email',
             'password'=>'required|min:4|max:30'
@@ -31,12 +36,43 @@ class LoginController extends Controller
 
              return redirect()->route('admin.dashboard');
          }else{
-             return redirect()->route('admin.login')->with('fail','Incorrect credentials');
+             RateLimiter::hit($this->throttleKey());
+             throw ValidationException::withMessages([
+                 'email' => __('auth.failed'),
+             ]);
+            //  return redirect()->route('admin.login')->with('fail','Incorrect credentials');
          }
+        //  RateLimiter::clear($this->authorize());
     }
 
-    function logout(){
+    public function logout(){
         auth()->guard()->logout();
         return redirect('/');
     }
+
+    public function throttleKey()
+    {
+        return Str::lower(request()->email).'|'.request()->ip();
+    }
+
+    public function ensureIsNotRateLimited(Request $request)
+    {
+        if(!RateLimiter::tooManyAttempts($this->throttleKey(), 3)) {
+            return;
+        }
+
+        event(new Lockout($request));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle',[
+                'seconds'  => $seconds,
+                'minutes'  => ceil($seconds/60),
+
+            ]),
+        ]);
+
+    }
+
 }
